63858cdaaaf635fd701d29bf5b54333d4eb6e31e
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-04-12 19:55:41 +0700

    Add files via upload

---
+++ Pertemuan 3/latihan2/Persegi/Persegi/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub btnHitung_Click(sender As Object, e As EventArgs) Handles btnHitung.Click
+        Dim sisi, luas, keliling As Integer
+        sisi = Val(txtSisi.Text)
+        luas = sisi ^ 2
+        keliling = 4 * sisi
+        txtLuas.Text = Str(luas)
+        txtKeliling.Text = Str(keliling)
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class

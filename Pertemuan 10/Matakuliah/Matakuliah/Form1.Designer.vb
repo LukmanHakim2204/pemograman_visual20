cbc01fce1d7d04bd83c68cd522d6d5493495f631
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2023-07-01 20:53:14 +0700

    Add files via upload

---
+++ Pertemuan 10/Matakuliah/Matakuliah/Form1.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSimpan = New System.Windows.Forms.Button()
+        Me.btnHapus = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtProdi = New System.Windows.Forms.TextBox()
+        Me.txtSKS = New System.Windows.Forms.TextBox()
+        Me.txtNamaMK = New System.Windows.Forms.TextBox()
+        Me.txtKodeMK = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSimpan
+        '
+        Me.btnSimpan.Location = New System.Drawing.Point(436, 68)
+        Me.btnSimpan.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnSimpan.Name = "btnSimpan"
+        Me.btnSimpan.Size = New System.Drawing.Size(105, 34)
+        Me.btnSimpan.TabIndex = 23
+        Me.btnSimpan.Text = "Simpan"
+        Me.btnSimpan.UseVisualStyleBackColor = True
+        '
+        'btnHapus
+        '
+        Me.btnHapus.Location = New System.Drawing.Point(436, 110)
+        Me.btnHapus.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnHapus.Name = "btnHapus"
+        Me.btnHapus.Size = New System.Drawing.Size(105, 34)
+        Me.btnHapus.TabIndex = 22
+        Me.btnHapus.Text = "Hapus"
+        Me.btnHapus.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(436, 152)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(105, 34)
+        Me.btnReset.TabIndex = 21
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(66, 192)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 17)
+        Me.Label4.TabIndex = 20
+        Me.Label4.Text = "Prodi"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(66, 146)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 17)
+        Me.Label3.TabIndex = 19
+        Me.Label3.Text = "SKS"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(66, 105)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(117, 17)
+        Me.Label2.TabIndex = 18
+        Me.Label2.Text = "Nama Matakuliah"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(66, 68)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 17)
+        Me.Label1.TabIndex = 17
+        Me.Label1.Text = "Kode Matakuliah"
+        '
+        'txtProdi
+        '
+        Me.txtProdi.Location = New System.Drawing.Point(191, 183)
+        Me.txtProdi.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtProdi.Name = "txtProdi"
+        Me.txtProdi.Size = New System.Drawing.Size(161, 22)
+        Me.txtProdi.TabIndex = 16
+        '
+        'txtSKS
+        '
+        Me.txtSKS.Location = New System.Drawing.Point(190, 142)
+        Me.txtSKS.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtSKS.Name = "txtSKS"
+        Me.txtSKS.Size = New System.Drawing.Size(163, 22)
+        Me.txtSKS.TabIndex = 15
+        '
+        'txtNamaMK
+        '
+        Me.txtNamaMK.Location = New System.Drawing.Point(190, 102)
+        Me.txtNamaMK.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtNamaMK.Name = "txtNamaMK"
+        Me.txtNamaMK.Size = New System.Drawing.Size(163, 22)
+        Me.txtNamaMK.TabIndex = 14
+        '
+        'txtKodeMK
+        '
+        Me.txtKodeMK.Location = New System.Drawing.Point(190, 65)
+        Me.txtKodeMK.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.txtKodeMK.Name = "txtKodeMK"
+        Me.txtKodeMK.Size = New System.Drawing.Size(163, 22)
+        Me.txtKodeMK.TabIndex = 13
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(35, 230)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(608, 265)
+        Me.DataGridView1.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(199, 520)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(303, 20)
+        Me.Label5.TabIndex = 24
+        Me.Label5.Text = "Lukman Hakim_201511030_TIF20C"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.GreenYellow
+        Me.ClientSize = New System.Drawing.Size(703, 569)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnSimpan)
+        Me.Controls.Add(Me.btnHapus)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtProdi)
+        Me.Controls.Add(Me.txtSKS)
+        Me.Controls.Add(Me.txtNamaMK)
+        Me.Controls.Add(Me.txtKodeMK)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "Form1"
+        Me.Text = "Matakuliah"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSimpan As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtProdi As TextBox
+    Friend WithEvents txtSKS As TextBox
+    Friend WithEvents txtNamaMK As TextBox
+    Friend WithEvents txtKodeMK As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label5 As Label
+End Class
